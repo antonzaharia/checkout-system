9df98dfb86d13e0940b90055afeb05bdb0cb6ec9
Anton Zaharia <zaharia_anton@yahoo.com>
2022-10-29 16:54:28 +0100

    BasketItem class

---
+++ lib/basket_item.rb
@@ -0,0 +1,18 @@
+class BasketItem
+  def initialize(item)
+    @item = item
+    @count = count
+  end
+
+  def discounted_price
+  end
+
+  def two_for_one
+  end
+
+  def half_price
+  end
+
+  def buy_3_get_1_free
+  end
+end
