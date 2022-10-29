6a2b132c5a5f27c5e6fc5e6262ce4a214ff9db16
Anton Zaharia <zaharia_anton@yahoo.com>
2022-10-29 17:42:36 +0100

    removed commented code

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -19,28 +19,6 @@ class Checkout
   end
 
   def total
-    # total = 0
-
-    # basket.inject(Hash.new(0)) { |items, item| items[item] += 1; items }.each do |item, count|
-    #   if item == :apple || item == :pear
-    #     if (count % 2 == 0)
-    #       total += prices.fetch(item) * (count / 2)
-    #     else
-    #       total += prices.fetch(item) * count
-    #     end
-    #   elsif item == :banana || item == :pineapple
-    #     if item == :pineapple
-    #       total += (prices.fetch(item) / 2)
-    #       total += (prices.fetch(item)) * (count - 1)
-    #     else
-    #       total += (prices.fetch(item) / 2) * count
-    #     end
-    #   else
-    #     total += prices.fetch(item) * count
-    #   end
-    # end
-
-    # total
     basket.calculate_total
   end
 
